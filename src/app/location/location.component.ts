6cd5a13691c49da996452996a75df28cdd15e9cf
Nikolas Rist <nikolas.rist@leanix.net>
2024-02-12 19:44:02 +0100

    Add hotels to location page

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './location.component.html',
   styleUrl: './location.component.sass',
 })
-export class LocationComponent {}
+export class LocationComponent {
+  // URL to your PDF file, can be local or remote
+  url = '/assets/Unterkuenfte_in_der_Region_WDM_mU20.pdf';
+
+  downloadPDF(): void {
+    // Create a new link element programmatically
+    const link = document.createElement('a');
+    // Set the href and download attributes of the link
+    link.href = this.url;
+    link.download = 'Unterkuenfte_in_der_Region_WDM_mU20.pdf'; // The file name for download
+    // Append the link to the body
+    document.body.appendChild(link);
+    // Simulate a click on the link
+    link.click();
+    // Remove the link from the body
+    document.body.removeChild(link);
+  }
+}
