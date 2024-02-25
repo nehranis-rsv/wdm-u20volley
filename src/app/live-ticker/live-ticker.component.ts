c9b6fc6e827662e48f4b7ec356da4ba4e9068be1
Nikolas Rist <nikolasrist@Nikolass-Laptop.fritz.box>
2024-02-25 22:27:54 +0100

    Add more details for match plans etc.

--- src/app/live-ticker/live-ticker.component.ts
+++ src/app/live-ticker/live-ticker.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { GroupComponent } from '../group/group.component';
 import { Group } from '../group/group.model';
+import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-live-ticker',
   standalone: true,
   templateUrl: './live-ticker.component.html',
   styleUrl: './live-ticker.component.sass',
-  imports: [MatTabsModule, GroupComponent],
+  imports: [
+    MatTabsModule,
+    GroupComponent,
+    CommonModule,
+    MatGridListModule,
+    MatButtonModule,
+  ],
 })
 export class LiveTickerComponent {
   GROUP_A: Group = {
@@ -45,4 +54,51 @@ export class LiveTickerComponent {
       'VfL Telstar Bochum',
     ],
   };
+
+  images = [
+    {
+      src: '/assets/spielplan_tag1.jpg',
+      alt: 'Spielplan Tag 1',
+      title: 'Spielplan Tag 1',
+    },
+    {
+      src: '/assets/spielplan_tag2.jpg',
+      alt: 'Spielplan Tag 2',
+      title: 'Spielplan Tag 2',
+    },
+    {
+      src: '/assets/spielplan_blanko.jpg',
+      alt: 'Spielplan Blanko',
+      title: 'Spielplan Blanko',
+    },
+  ];
+
+  gridColumns = 3;
+  selectedImage: string | null = null;
+
+  openImage(src: string) {
+    if (this.selectedImage === src) {
+      // If the same image is clicked, hide the large view
+      this.selectedImage = null;
+    } else {
+      // Show the clicked image
+      this.selectedImage = src;
+    }
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      this.selectedImage = null;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth < 600) {
+      this.gridColumns = 2; // Adjust for small screens
+    } else {
+      this.gridColumns = 3; // Default
+    }
+  }
 }
